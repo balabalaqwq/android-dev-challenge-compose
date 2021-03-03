3ad88de97828e12af131e6dd9c944a93a4654d0c
balabalaqwq <39816531+balabalaqwq@users.noreply.github.com>
2021-03-03 11:24:28 +0800

    commit README.md

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -21,11 +21,20 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -108,11 +117,17 @@ fun MyApp() {
             )
         )
         LazyColumn {
-            items(catList, ({
-                it.toString()
-            }), itemContent = {
-                CatItem(it)
-            })
+            items(
+                catList,
+                (
+                    {
+                        it.toString()
+                    }
+                    ),
+                itemContent = {
+                    CatItem(it)
+                }
+            )
         }
     }
 }
